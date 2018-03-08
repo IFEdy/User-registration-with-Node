507f93e179fcb41eba95bf6b4716e7b7682d429f
ADEYEMI IFEDAYO O <adeyemiife@ymail.com>
2018-03-08 11:26:52 -0800

    node

--- route/index.js
+++ route/index.js
@@ -1,2 +1,14 @@
 var express = require('express');
-var Router = express.router();
+var router = express.Router();
+
+router.get('/', function(req, res, next){
+    res.render("home", {title: 'Home'});
+});
+
+router.get('/contact', function(req, res, next){
+   res.render('contact', {title: "Contact"});
+});
+
+router.get('/about', function(req, res, next){
+   res.render('about', {title: "About-Us"});
+});
