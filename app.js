e834e3b185a009c677cf7dec043cac1660b7c0ca
ADEYEMI IFEDAYO O <adeyemiife@ymail.com>
2018-03-08 10:51:36 -0800

    made by me

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express.app;
 
-app.use(function(res, req){
+function(req, res){
     res.send("We are on Express");
 
-});
+};
 
 app.listen(3000, function (){
     console.log("Listening to this App @ 3000");
