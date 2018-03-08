c8f9823f4d59ea1d79471769da9dbf19e2766633
ADEYEMI IFEDAYO O <adeyemiife@ymail.com>
2018-03-08 11:21:23 -0800

    node

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var bodyparser = require("body-parser");
 
+app.get(function(req, res){
+  res.send('this an express app');
+});
 app.listen(8000, function (){
     console.log("Listening to this App @ 8000");
 });
