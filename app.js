974305686a22290df05612b4a3e1d2bf0705b9d1
ADEYEMI IFEDAYO O <adeyemiife@ymail.com>
2018-03-08 14:10:44 -0800

    made by me

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var app = express();
-var bodyparser = require("body-parser");
-var router = require(".../route/index.js");
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(__dirname + '/public'));
+                       
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/view');
+
+var routes = require("./route/index");
+app.use('/', routes);
 
 
 app.listen(3000, function (){
--- route/index.js
+++ route/index.js
@@ -1,27 +1,27 @@
 var express = require('express');
-var router = express.Router();
-var exports = module.exports[];
+var routes = express.Router();
+// var exports = module.exports = {};
 
-router.get('/register', function(req, res, next){
+routes.get('/register', function(req, res, next){
    //return res.render("register", {title: "Register"});
     return res.send('register hia!');
 });
 
-router.post('/register', function(req, res, next){
+routes.post('/register', function(req, res, next){
    return res.send('User Created!');
 });
 
-router.get('/', function(req, res, next){
+routes.get('/', function(req, res, next){
     return res.render("home", {title: 'Home'});
 });
 
-router.get('/contact', function(req, res, next){
+routes.get('/contact', function(req, res, next){
    return res.render('contact', {title: "Contact"});
 });
 
-router.get('/about', function(req, res, next){
+routes.get('/about', function(req, res, next){
    return res.render('about', {title: "About-Us"});
 });
 
 
-exports = router;
+module.exports = routes;
