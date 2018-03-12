8e6eceb66b8058552f93f710d793b05e382382fb
ADEYEMI IFEDAYO O <adeyemiife@ymail.com>
2018-03-12 14:19:09 -0700

    made by me

--- route/index.js
+++ route/index.js
@@ -10,24 +10,36 @@ routes.get('/register', function(req, res, next){
 
 routes.post('/register', function(req, res, next){
 //    return res.send('User Created!');
+    //To all fields are filled
     if (req.body.Email && 
     req.body.Name &&
     req.body.FavouriteBook &&
     req.body.password &&
     req.body.confirmpassword
     ){
+        //to check if both password and confirm password are same
         if( req.body.password !== req.body.confirmpassword){
             var err = new Error('Passwords do not match');
             err.status = 400;
             return next(err);
         }
+        //creating a variable that stores user's data
+        var UserData ={
+            Email : req.body.Email,
+            Name : req.body.Name,
+            FavouriteBook : req.body.FavouriteBook,
+            password : req.body.password
+        }
+
      } else{
+         //Error created to ensure all fields are filled...
         var err = new Error('All fields required to be filled....');
         err.status = 400;
         return next(err);
     }
 });
 
+//To take user's to the home page when Home button is clicked on the nav
 routes.get('/', function(req, res, next){
     return res.render("home", {title: 'Home'});
 });
